cf1728e0fda1d9ee9e71be7d4657564bf11ffe52
Anshal <104653839+Anshalll@users.noreply.github.com>
2024-10-31 13:41:33 +0530

    Implementing Register user

---
+++ register.h
@@ -0,0 +1,21 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <string>
+using namespace std;
+
+class Register
+{
+public:
+    struct
+    {
+        int age;
+        string firstname;
+        string lastname;
+        string dob;
+        
+
+    } userdetails;
+};
+
+#endif
