e6e0349546eeebb6b5852f1cf098e5ad46c7d07b
Anshal <104653839+Anshalll@users.noreply.github.com>
2024-10-31 13:51:23 +0530

    Bug fixes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Main{
+    public: 
+        Main(){
+
+            static int option;
+            cout << "Please select option \n1.Register \n2.Login" << endl;
+            cout << "Select an option ";
+            cin >> option;
+        }  
+};
+
 int main(){
-    
+    Main mainobj;
+
     return 0;
 }
